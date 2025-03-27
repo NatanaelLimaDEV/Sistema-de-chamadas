66ffc8b0b6de6b659dbc89476e14921d72c42784
NatanaelLimaDEV <natanaellimaofc@gmail.com>
2025-03-27 18:33:40 -0300

    Projeto chamados finalizado

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,16 +1,108 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../contexts/auth";
 import Header from "../../components/Header";
 import "./dashboard.css";
 import Title from "../../components/Title";
 import { FiEdit2, FiMessageSquare, FiPlus, FiSearch } from "react-icons/fi";
 import { Link } from "react-router-dom";
+import {
+  collection,
+  getDocs,
+  orderBy,
+  limit,
+  startAfter,
+  query,
+} from "firebase/firestore";
+import { db } from "../../services/firebaseConnection";
+import { format } from "date-fns";
+import Modal from "../../components/Modal";
+
+const listRef = collection(db, "chamados");
 
 export default function Dashboard() {
-  const { logout } = useContext(AuthContext);
+  const [chamados, setChamados] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [isEmpty, setIsEmpty] = useState(false);
+  const [lastDocs, setLastDocs] = useState()
+  const [loadingMore, setLoadingMore] = useState(false)
+  const [showPostModal, setShowPostModal] = useState(false)
+  const [detail, setDetail] = useState()
 
-  async function handleLogout() {
-    await logout();
+  useEffect(() => {
+    async function loadChamados() {
+      const q = query(listRef, orderBy("created", "desc"), limit(5));
+      const querySnapshot = await getDocs(q);
+      setChamados([])
+
+      await updateState(querySnapshot);
+
+      setLoading(false);
+    }
+
+    loadChamados();
+
+    return () => {};
+  }, []);
+
+  async function updateState(querySnapshot) {
+    const isCollectionEmpty = querySnapshot.size === 0;
+
+    if (!isCollectionEmpty) {
+      let lista = [];
+
+      querySnapshot.forEach((doc) => {
+        lista.push({
+          id: doc.id,
+          assunto: doc.data().assunto,
+          cliente: doc.data().cliente,
+          clienteId: doc.data().clienteId,
+          created: doc.data().created,
+          createdFormat: format(doc.data().created.toDate(), "dd/MM/yyyy"),
+          status: doc.data().status,
+          complemento: doc.data().complemento,
+        });
+      });
+
+      const lastDoc = querySnapshot.docs[querySnapshot.docs.length - 1]
+
+      setChamados((chamados) => [...chamados, ...lista]);
+      setLastDocs(lastDoc)
+    } else {
+      setIsEmpty(true);
+    }
+
+    setLoadingMore(false)
+  }
+
+  async function handleMore(){
+    setLoadingMore(true)
+
+    const q = query(listRef, orderBy("created", "desc"), startAfter(lastDocs), limit(5));
+    const querySnapshot = await getDocs(q)
+    await updateState(querySnapshot)
+  }
+
+  function toggleModal(item){
+    setShowPostModal(!showPostModal)
+    setDetail(item)
+  }
+
+  if (loading) {
+    return (
+      <div>
+        <Header />
+
+        <div className="content">
+          <Title name="Tickets">
+            <FiMessageSquare size={25} />
+          </Title>
+
+          <div className="container dashboard">
+            <span>Buscando chamados...</span>
+          </div>
+        </div>
+      </div>
+    );
   }
 
   return (
@@ -23,64 +115,81 @@ export default function Dashboard() {
         </Title>
 
         <>
-          <Link to="/new" className="new">
-            <FiPlus color="#fff" size={25} />
-            Novo chamado
-          </Link>
+          {chamados.length === 0 ? (
+            <div className="container dashboard">
+              <span>Nenhum chamado encontrado...</span>
+              <Link to="/new" className="new">
+                <FiPlus color="#fff" size={25} />
+                Novo chamado
+              </Link>
+            </div>
+          ) : (
+            <>
+              <Link to="/new" className="new">
+                <FiPlus color="#fff" size={25} />
+                Novo chamado
+              </Link>
 
-          <table>
-            <thead>
-                <tr>
+              <table>
+                <thead>
+                  <tr>
                     <th scope="col">Cliente</th>
                     <th scope="col">Assunto</th>
                     <th scope="col">Status</th>
                     <th scope="col">Cadastrado em</th>
                     <th scope="col">#</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td data-label="Cliente">Mercado Esquina</td>
-                    <td data-label="Assunto">Suporte</td>
-                    <td data-label="Status">
-                        <span className="badge" style={{backgroundColor: '#999'}}>
-                            Em aberto
-                        </span>
-                    </td>
-                    <td data-label="Cadastrado">12/05/2022</td>
-                    <td data-label="#">
-                        <button className="action" style={{ backgroundColor: '#3583f6' }}>
+                  </tr>
+                </thead>
+                <tbody>
+                  {chamados.map((item, index) => {
+                    return (
+                      <tr key={index}>
+                        <td data-label="Cliente">{item.cliente}</td>
+                        <td data-label="Assunto">{item.assunto}</td>
+                        <td data-label="Status">
+                          <span
+                            className="badge"
+                            style={{ backgroundColor: item.status === "Aberto" ? "#5cb85c" : "#999" }}
+                          >
+                            {item.status}
+                          </span>
+                        </td>
+                        <td data-label="Cadastrado">{item.createdFormat}</td>
+                        <td data-label="#">
+                          <button
+                            className="action"
+                            style={{ backgroundColor: "#3583f6" }}
+                            onClick={() => toggleModal(item)}
+                          >
                             <FiSearch color="#fff" size={17} />
-                        </button>
-                        <button className="action" style={{ backgroundColor: '#f6a935' }}>
+                          </button>
+                          <Link
+                          to={`/new/${item.id}`}
+                            className="action"
+                            style={{ backgroundColor: "#f6a935" }}
+                          >
                             <FiEdit2 color="#fff" size={17} />
-                        </button>
-                    </td>
-                </tr>
+                          </Link>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
 
-                
-                <tr>
-                    <td data-label="Cliente">Mercado Esquina</td>
-                    <td data-label="Assunto">Suporte</td>
-                    <td data-label="Status">
-                        <span className="badge" style={{backgroundColor: '#999'}}>
-                            Em aberto
-                        </span>
-                    </td>
-                    <td data-label="Cadastrado">12/05/2022</td>
-                    <td data-label="#">
-                        <button className="action" style={{ backgroundColor: '#3583f6' }}>
-                            <FiSearch color="#fff" size={17} />
-                        </button>
-                        <button className="action" style={{ backgroundColor: '#f6a935' }}>
-                            <FiEdit2 color="#fff" size={17} />
-                        </button>
-                    </td>
-                </tr>
-            </tbody>
-          </table>
+              {loadingMore && <h3>Buscando mais chamados...</h3>}
+              {!loadingMore && !isEmpty && <button onClick={handleMore} className="btn-more">Buscar mais</button>}
+            </>
+          )}
         </>
       </div>
+
+      {showPostModal && (
+        <Modal
+          conteudo={detail}
+          close={() => setShowPostModal(!showPostModal)}
+        />
+      )}
     </div>
   );
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ import Dashboard from "../pages/Dashboard"
 import Private from "./Private"
 import Profile from "../pages/Profile"
 import Customers from "../pages/Customers"
+import New from "../pages/New"
 
 
 
@@ -17,6 +18,8 @@ function RoutesApp() {
             <Route path="/dashboard" element={ <Private><Dashboard/></Private> } />
             <Route path="/profile" element={ <Private><Profile/></Private> } />
             <Route path="/customers" element={ <Private><Customers/></Private> } />
+            <Route path="/new" element={ <Private><New/></Private> } />
+            <Route path="/new/:id" element={ <Private><New/></Private> } />
         </Routes>
     )
 }
